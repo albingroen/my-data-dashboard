9c3f37b94500d9fbc81f4976ff0725fd6046e52c
Albin Groen <albin.groen@gmail.com>
2018-11-23 22:54:55 +0200

    Continued on header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,29 @@
 import { h, Component } from "preact";
-import { Container } from "@wopify/ui-design";
+import axios from "axios";
+import { H3, Button, Container } from "@wopify/ui-design";
 import { Link } from "preact-router";
 import style from "./style.less";
 
 export default class Header extends Component {
+	handleAuth() {
+		console.log("handleAuth()");
+
+		axios
+			.post("http://localhost:8080/auth/google")
+			.then(res => {
+				console.log(res);
+			})
+			.catch(err => console.log(err));
+	}
+
 	render() {
 		return (
 			<header class={style.header}>
-				<h1>Preact App</h1>
 				<nav>
-					<Link href="/">Home</Link>
+					<H3>MyData™</H3>
+					<Button onClick={() => this.handleAuth()} primary>
+						Login with Google
+					</Button>
 				</nav>
 			</header>
 		);
