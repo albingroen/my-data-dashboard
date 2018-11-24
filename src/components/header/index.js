27cc9844fcc2689ea720d3a68cb7a811e0bdbcdd
Marco <marcohuttunen@gmail.com>
2018-11-24 13:50:45 +0200

    new url

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -18,8 +18,7 @@ export default class Header extends Component {
 
 	render() {
 		let url =
-			"https://accounts.google.com/signin/oauth/oauthchooseaccount?client_id=120720206824-cibobt957sd9rtmlu4l71toorl15ngap.apps.googleusercontent.com&as=Gu3Au4zrVq2c8Ckw2wNk5Q&destination=http%3A%2F%2Flocalhost%3A8080&approval_state=!ChRuV1l5RGIyUzFrOUdEZVN0bU1lWRIfTThicEo2RFBXNThSOERFdWhZOThQYzkxZElSTGRCWQ%E2%88%99APNbktkAAAAAW_pVqo_lVuMmi0JeJEx4E-6uVe4FOyR1&oauthgdpr=1&xsrfsig=AHgIfE-Eop4e_R-Gjd0S40h_cs6oJOzAMQ&flowName=GeneralOAuthFlow";
-
+			"https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fgmail.readonly&response_type=code&client_id=120720206824-cibobt957sd9rtmlu4l71toorl15ngap.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth%2Fgoogle%2Fcallback";
 		return (
 			<header class={style.header}>
 				<nav>
